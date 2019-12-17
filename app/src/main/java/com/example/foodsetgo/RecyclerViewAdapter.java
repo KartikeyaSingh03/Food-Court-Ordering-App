3a3a22cd8857e07cbbbc515e18403c2e02cf47f2
Kartikeya Singh <kartikeya.singh030600@gmail.com>
2019-12-17 17:43:44 +0530

    Menu Activity Added

--- app/src/main/java/com/example/foodsetgo/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/foodsetgo/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.foodsetgo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,7 +45,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.parentLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext,mImageName.get(position),Toast.LENGTH_LONG).show();
+                Intent i = new Intent(mContext,restaurantMenu.class);
+                view.getContext().startActivity(i);
             }
         });
     }
---
+++ app/src/main/java/com/example/foodsetgo/restaurantMenu.java
@@ -0,0 +1,14 @@
+package com.example.foodsetgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurantMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_menu);
+    }
+}
