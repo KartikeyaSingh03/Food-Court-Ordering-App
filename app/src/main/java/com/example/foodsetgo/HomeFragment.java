aa432f2c86aa2db4c0ec07a8c131c4c9551d4c75
Kartikeya Singh <kartikeya.singh030600@gmail.com>
2019-12-17 17:37:33 +0530

    Recycler View Implemented

--- app/src/main/java/com/example/foodsetgo/HomeFragment.java
+++ app/src/main/java/com/example/foodsetgo/HomeFragment.java
@@ -7,12 +7,39 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrl = new ArrayList<>();
+    View Viewroot;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        Viewroot=inflater.inflate(R.layout.fragment_home, container, false);
+        initImageBitmaps();
+        return Viewroot;
     }
 
+    private void initImageBitmaps(){
+        mImageUrl.add("https://www.gstatic.com/webp/gallery/1.jpg");
+        mNames.add("Norway");
+
+        mImageUrl.add("https://www.gstatic.com/webp/gallery/2.jpg");
+        mNames.add("Kayaker");
+
+        mImageUrl.add("https://www.gstatic.com/webp/gallery/4.jpg");
+        mNames.add("Cherry");
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = Viewroot.findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mImageUrl,mNames,getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 }
---
+++ app/src/main/java/com/example/foodsetgo/RecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.foodsetgo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private ArrayList<String> mImage= new ArrayList<>();
+    private ArrayList<String> mImageName= new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mImage, ArrayList<String> mImageName, Context mContext) {
+        this.mImage = mImage;
+        this.mImageName = mImageName;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Glide.with(mContext).asBitmap().load(mImage.get(position)).into(holder.image);
+        holder.imageName.setText(mImageName.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,mImageName.get(position),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageName.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+        public ViewHolder(View itemView){
+            super(itemView);
+            image =itemView.findViewById(R.id.image);
+            imageName=itemView.findViewById(R.id.image_name);
+            parentLayout=itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
